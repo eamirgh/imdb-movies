9a3ef70e251e74ce4510098852f7e4a060f41885
Amir Ghaffari <a.g.h.94.72@gmail.com>
2025-06-02 15:59:05 +0330

    wip

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,24 @@ import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from "path"
+import browserslist from 'browserslist';
+import {browserslistToTargets} from 'lightningcss';
 
 // https://vite.dev/config/
 export default defineConfig({
+    css: {
+        transformer: 'lightningcss',
+        lightningcss: {
+            targets: browserslistToTargets(browserslist('>= 0.25%'))
+        }
+    },
+    build: {
+        cssMinify: 'lightningcss',
+    },
+    esbuild: {
+        legalComments: 'external'
+    },
+    emptyOutDir: true,
     plugins: [
         react(),
         tailwindcss(),
